e9c34f5af4c77d9d19a00bb840c7c99872c94691
agniesza <agnieszka.gfkm@o2.pl>
2025-06-13 21:34:21 +0200

    enum refactor

--- src/spacex/main/rocket/RocketStatus.java
+++ src/spacex/main/rocket/RocketStatus.java
@@ -1,16 +1,8 @@
 package spacex.main.rocket;
 
-public enum RocketStatus {
-    /*
-    Initial status, where the rocket is not assigned to any mission
-     */
+ public enum RocketStatus {
+
     ON_GROUND,
-    /*
-    The rocket was assigned to the mission
-     */
     IN_SPACE,
-    /*
-    The rocket is due to repair, it implies “Pending” status of the mission
-     */
     IN_REPAIR
 }
